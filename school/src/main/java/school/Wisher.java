7b70004271555f3a7c9942073a0cc147a351695a
pavankumar1809 <paulpavan_k@hotmail.com>
2016-10-11 21:24:41 +0530

    ok

--- school/src/main/java/school/Wisher.java
+++ school/src/main/java/school/Wisher.java
@@ -2,6 +2,6 @@ package school;
 
 public class Wisher {
 public static void main(String[] args) {
-	System.out.println("hi hello, iam from mumbai not yar from bnglr");
+	System.out.println("hi hello, iam from mumbai not yar from bnglr and from hyd");
 }
 }
